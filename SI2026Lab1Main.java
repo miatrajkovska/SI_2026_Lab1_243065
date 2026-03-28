bcb464bc436a57013619a3e198b97d993a94b7b3
mia <trajkovskamia1@gmail.com>
2026-03-28 12:16:29 +0100

    Update borrow message on master

--- SI2026Lab1Main.java
+++ SI2026Lab1Main.java
@@ -60,13 +60,13 @@ class Library {
     	return false;
     }
 
-    // TODO: Implement in branch feature-borrow-book
+    
     public void borrowBook(String title) {
         for (Book book : books) {
             if (book.getTitle().equalsIgnoreCase(title)) {
                 if (!book.isBorrowed()) {
                     book.setBorrowed(true);
-                    System.out.println("Book borrowed.");
+                    System.out.println("Book checked out");
                 } else {
                     System.out.println("Book is already borrowed.");
                 }
